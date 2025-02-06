a3120cad56d475fa8d8e3e06227bbab59c5e4caa
Wislla <wislla21@gmail.com>
2025-02-06 15:10:06 -0300

    first commit

--- src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -10,18 +10,18 @@ import { Component } from '@angular/core';
 export class ListarPensamentoComponent {
   listaPensamentos = [
     {
-      conteudo: "I love Angular",
-      autoria: "Wislla",
+      conteudo: "Em algumas agências no 90 circulou um texto chamado sensata substituir Lorem Ipsum para dar um visual mais moderno para o conteúdo. Mas muitas pessoas estavam olhando para ler o texto quando ele estava em Francês ou Inglês, o efeito desejado não foi alcançado. Trabalhar com texto legível, contendo instruções pode causar distrações e isso pode ajudar a focar o layout. A vantagem de origem latina e conteúdo absurdo Lorem ipsum impede o leitor de se distrair com o conteúdo do texto e, portanto, pode se concentrar a sua atenção no design gráfico. Na verdade, o texto Lorem Ipsum tem a vantagem, em contraste com um texto genérico usando comprimento de palavra variável para simular uma ocupação normal do modelo de modo a que ele corresponde ao produto final e para garantir a futura publicação inalterado.",
+      autoria: "Wislla1",
       modelo: "modelo3"
     },
     {
       conteudo: "I love Angular",
-      autoria: "Wislla",
+      autoria: "Wislla2",
       modelo: "modelo3"
     },
     {
       conteudo: "I love Angular",
-      autoria: "Wislla",
+      autoria: "Wislla,",
       modelo: "modelo3"
     }
   ];
--- src/app/components/pensamentos/pensamento/pensamento.component.ts
+++ src/app/components/pensamentos/pensamento/pensamento.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-pensamento',
@@ -8,9 +8,15 @@ import { Component } from '@angular/core';
   styleUrl: './pensamento.component.css'
 })
 export class PensamentoComponent {
-  pensamento = {
+  @Input() pensamento = {
     conteudo: "I love Angular",
     autoria: "Wislla",
     modelo: "modelo3"
+  }
+  larguraPensamento(): string {
+    if(this.pensamento.conteudo.length > 256){
+      return "pensamento-g"
+    }
+    return "pensamento-p";
   }
 }
